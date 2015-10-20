441cbcc85ce21132fad22830b883674b2b030f1c
netmanchris <chris_young@me.com>
2015-10-20 15:53:02 -0400

    Added Mac-auth to the template

--- Provision Clearpass Config/Render_template.py
+++ Provision Clearpass Config/Render_template.py
@@ -5,19 +5,10 @@ import yaml
 
 #This section is for testing only
 
-network_global = yaml.load('''radius_server:
-- {server_ip: 10.101.0.200, auth_key: my_shared_secret}
-- {server_ip: 10.101.0.201, auth_key: my_shared_secret}
-- {server_ip: 10.101.0.202, auth_key: my_shared_secret}
-dhcp_server:
-- {server_ip: 10.3.10.2}
-- {server_ip: 10.101.254.1}
-- {server_ip: 10.101.0.202}
-dhcp_snooping_vlans:
-    vlans: [2, 3, 4, 5, 6]
-trust_port_list:
-- interface: "1/23"
-- interface: "1/24"''')
+
+with open("Provision_Clearpass_Input.yaml") as netglobal:
+    network_global =  yaml.load(netglobal)
+
 
 ENV = Environment(loader=FileSystemLoader('./'))
 
@@ -25,3 +16,5 @@ ENV = Environment(loader=FileSystemLoader('./'))
 # Render template and print generated config to console
 template = ENV.get_template("Provision_ClearPass_Config.j2")
 print (template.render(network_global=network_global))
+
+
