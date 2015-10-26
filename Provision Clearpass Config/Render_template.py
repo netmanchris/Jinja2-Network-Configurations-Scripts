845756d027b5b73c19b9203c5de45f37c51e4ced
netmanchris <chris_young@me.com>
2015-10-26 09:29:55 -0400

    Added export functionality

--- Provision Clearpass Config/Render_template.py
+++ Provision Clearpass Config/Render_template.py
@@ -6,21 +6,19 @@ import yaml, datetime
 #This section is for testing only
 
 
-with open("Provision_Clearpass_Input.yaml") as netglobal:
-    network_global =  yaml.load(netglobal)
+def main():
+    with open("Provision_Clearpass_Input.yaml") as netglobal:
+        network_global =  yaml.load(netglobal)
+    ENV = Environment(loader=FileSystemLoader('./'))
+    # Render template and print generated config to console
+    template = ENV.get_template("Provision_ClearPass_Config.j2")
+    current_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d %H%M%S')
+    config_name = str("Provision_Clearpass"+current_date+".txt")
+    with open(config_name, "w") as text_file:
+        print (template.render(network_global=network_global), file=text_file)
 
 
-ENV = Environment(loader=FileSystemLoader('./'))
 
 
-# Render template and print generated config to console
-template = ENV.get_template("Provision_ClearPass_Config.j2")
-
-
-#481
-current_date = datetime.datetime.strftime(datetime.datetime.now(), '%Y%m%d %H%M%S')
-config_name = str("Provision_Clearpass"+current_date+".txt")
-with open(config_name, "w") as text_file:
-    print (template.render(network_global=network_global), file=text_file)
-
-
+if __name__ == "__main__":
+    main()
